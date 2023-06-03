ec19aedbacb438c015e8996168afa1fba4653423
Caio Garcia <caiopassador@hotmail.com>
2023-06-03 15:50:26 +0100

    feat: gitignore + dotenv + pg

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
+require("dotenv").config({ path: "src" + "/.env" });
+
 const express = require("express");
 const app = express();
-const port = 3000;
 
 app.get("/", (req: any, res: any): any => {
   res.send("Nene!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(Number(process.env.PORT), () => {
+  console.log("Server up at port: ", process.env.PORT);
 });
